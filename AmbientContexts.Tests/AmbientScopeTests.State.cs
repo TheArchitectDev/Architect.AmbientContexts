974b906ae1a19fedc55fec64b975a1045e159798
Timovzl <timovzl@gmail.com>
2020-03-22 19:43:49 +0100

    - Version 1.0.0-RC1. - Major rework, for correctness. (Primarily protected against throwing from subclass constructor.) - Activate() must now be called to activate a non-default instance. - Now netcoreapp3.1 (instead of netstandard2.0) in order to Enable Nullable. (Could use netcoreapp2.1 or netstandard2.1, theoretically.)

---
+++ AmbientContexts.Tests/AmbientScopeTests.State.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+
+namespace Architect.AmbientContexts.Tests
+{
+	public sealed partial class AmbientScopeTests
+	{
+		[Fact]
+		public void Construct_Regularly_ShouldResultInStateNew()
+		{
+			using var scope = new ManuallyActivatedScope(1, AmbientScopeOption.ForceCreateNew);
+			Assert.Equal(AmbientScopeState.New, scope.State);
+		}
+		
+		[Fact]
+		public void Activate_Regularly_ShouldResultInStateActive()
+		{
+			using var scope = new ManuallyActivatedScope(1, AmbientScopeOption.ForceCreateNew);
+
+			scope.Activate();
+
+			Assert.Equal(AmbientScopeState.Active, scope.State);
+		}
+		
+		[Fact]
+		public void Construct_WithActivateFromConstructor_ShouldResultInStateActive()
+		{
+			using var scope = new TestScope(1, AmbientScopeOption.ForceCreateNew);
+
+			Assert.Equal(AmbientScopeState.Active, scope.State);
+		}
+		
+		[Fact]
+		public void Dispose_FromStateNew_ShouldResultInStateDisposed()
+		{
+			using var scope = new ManuallyActivatedScope(1, AmbientScopeOption.ForceCreateNew);
+
+			scope.Dispose();
+
+			Assert.Equal(AmbientScopeState.Disposed, scope.State);
+		}
+		
+		[Fact]
+		public void Dispose_FromStateActive_ShouldResultInStateDisposed()
+		{
+			using var scope = new TestScope(1, AmbientScopeOption.ForceCreateNew);
+
+			scope.Dispose();
+
+			Assert.Equal(AmbientScopeState.Disposed, scope.State);
+		}
+		
+		[Fact]
+		public void Dispose_TwiceFromStateActive_ShouldResultInStateDisposed()
+		{
+			using var scope = new TestScope(1, AmbientScopeOption.ForceCreateNew);
+
+			scope.Dispose();
+			scope.Dispose();
+
+			Assert.Equal(AmbientScopeState.Disposed, scope.State);
+		}
+	}
+}
